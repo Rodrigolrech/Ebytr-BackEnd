6c6f38a402eb97d3049623c58d14b7ee5cea4ce7
Rodrigo Rech <rodrigorech92@gmail.com>
2021-11-01 08:01:05 -0300

    Feat: Connection with mongodb

---
+++ models/connection.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Ebytr';
+
+let schema = null;
+
+async function getConnection() {
+  if (schema) return Promise.resolve(schema);
+  return MongoClient
+    .connect(MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => conn.db('Ebytr'))
+    .then((dbSchema) => {
+      schema = dbSchema;
+      return schema;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+module.exports = { getConnection };
