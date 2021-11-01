4a07982a80c3130a9a7c3ba5a597cc98036e37c5
Rodrigo Rech <rodrigorech92@gmail.com>
2021-11-01 12:46:42 -0300

    Feat: app.post('/user') to create an user

--- src/app.js
+++ src/app.js
@@ -2,13 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { checkLogin } = require('./middlewares/checkLogin')
+const { 
+  checkUserEmail,
+  checkUserPassword,
+  checkUserUsername,
+  checkUserRole
+} = require('./middlewares/createUser');
 const usersControllers = require('./controllers/usersControllers')
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/', checkLogin, usersControllers.login)
-
+app.post('/', checkLogin, usersControllers.login);
+app.post('/user',
+  checkUserEmail,
+  checkUserPassword,
+  checkUserRole,
+  checkUserUsername,
+  usersControllers.createUser
+)
 
 const PORT = 3000;
 
