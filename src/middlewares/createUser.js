d83048ebba308719d27a2561c3306724fc49b814
Rodrigo Rech <rodrigorech92@gmail.com>
2021-11-01 12:29:23 -0300

    Feat: createUser middleware with checkUserEmail, checkUserUsername, checkUserPassword and checkUserRole

---
+++ src/middlewares/createUser.js
@@ -0,0 +1,47 @@
+const checkUserEmail = (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'Invalid entries. Email not informed.'})
+  }
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    .test(email)) return res.status(400).json({ message: 'Invalid entries. Email in wrong format.'});
+  if (await usersModel.getByEmail(email)) {
+      res.status(409).json({ message: 'Email already registered' });
+  }
+  next();
+};
+
+const checkUserUsername = (req, res) => {
+  const { username } = req.body;
+  if (!username || username.length <= 6) {
+    return res.status(400).json({ message: 'Invalid entries. Username length must be at least 6'})
+  };
+  next();
+};
+
+const checkUserPassword = (req, res) => {
+  const { password } = req.body;
+  if(!password || password.length <= 6) {
+    return res.status(400).json({ message: 'Invalid entries. Username length must be at least 6'})
+  };
+  next();
+};
+
+const checkUserRole = (req, res) => {
+  const { role } = req.body;
+  if(!role) {
+    return res.status(400).json({ message: 'Invalid entries. Role not informed.'})
+  };
+  if(role != 'Developer' || role != 'Scrum Master' || role != 'Project owner') {
+    return res.status(400)
+      .json({ message: 'Invalid entries. Role must be "Developer" or "Scrum Master" or "Project owner".'})
+  }
+  next();
+}
+
+module.exports = {
+  checkUserEmail,
+  checkUserUsername,
+  checkUserPassword,
+  checkUserRole,
+};
