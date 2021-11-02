f77e756b62217e056a4fef99b02144584c93b676
Rodrigo Rech <rodrigorech92@gmail.com>
2021-11-01 23:24:25 -0300

    Feat: toDoListService.getAllTasks implemented

--- src/services/toDoListService.js
+++ src/services/toDoListService.js
@@ -11,7 +11,13 @@ const deleteTask = async (_id) => {
   return { status: 201, message: { message: `Task with id ${_id} deleted` } };
 };
 
+const getAllTasks = async (sorted) => {
+  const tasks = await toDoListModel.getAllTasks(sorted);
+  return { status: 200, message: tasks };
+};
+
 module.exports = {
   insertNewTask,
   deleteTask,
+  getAllTasks,
 };
