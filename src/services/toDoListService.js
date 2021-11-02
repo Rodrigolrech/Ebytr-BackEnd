8c7677040c400718b8b444e7efc4541ecb13be84
Rodrigo Rech <rodrigorech92@gmail.com>
2021-11-01 22:53:35 -0300

    Feat: toDoListService.deleteTask implemented

--- src/services/toDoListService.js
+++ src/services/toDoListService.js
@@ -6,6 +6,12 @@ const insertNewTask = async (taskDescription, status, creatorUser, createdDate) 
   return { status: 201, message };
 };
 
+const deleteTask = async (_id) => {
+  await toDoListModel.deleteTask(_id);
+  return { status: 201, message: { message: `Task with id ${_id} deleted` } };
+};
+
 module.exports = {
   insertNewTask,
+  deleteTask,
 };
