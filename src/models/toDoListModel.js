80831e972da9ef8ae1c1206b4cd1c337713dab87
Rodrigo Rech <rodrigorech92@gmail.com>
2021-11-01 21:03:07 -0300

    Feat: toDoListModel.insertNewTask implemented

---
+++ src/models/toDoListModel.js
@@ -0,0 +1,17 @@
+const mongoConnection = require('./connection');
+
+const insertNewTask = async (taskDescription, status, creatorUser, createdDate) => {
+  const getCollection = await mongoConnection.getConnection()
+    .then((db) => db.collection('users'));
+  const { insertedId: _id } = await getCollection.insertOne({
+    taskDescription, status, creatorUser, createdDate,
+  });
+  const newTask = {
+    _id, taskDescription, status, creatorUser, createdDate,
+  };
+  return newTask;
+};
+
+module.exports = {
+  insertNewTask,
+};
