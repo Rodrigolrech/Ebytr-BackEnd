5a572e48302b62d541288f6a656cff909bd86621
Rodrigo Rech <rodrigorech92@gmail.com>
2021-11-01 21:55:14 -0300

    Feat: middlewares createNewTask implemented checking taskDescription and status

---
+++ src/middlewares/createNewTask.js
@@ -0,0 +1,25 @@
+const checkTaskDescription = (req, res, next) => {
+  const { taskDescription } = req.body;
+  if (!taskDescription) return res.status(400).json({ message: 'Invalid entries. taskDescription required' });
+  if (taskDescription.length <= 10) {
+    return res.status(400)
+      .json({ message: 'Invalid entries. Task description must have at least 10 characters' });
+  }
+  return next();
+};
+
+const checkStatus = (req, res, next) => {
+  const { status } = req.body;
+  if (!status) {
+    req.body.status = 'Pendente';
+  }
+  if (status === 'Pendente' || status === 'em andamento' || status === 'Pronto') {
+    return next();
+  }
+  return res.status(400).json({ message: 'Invalid entries. Status has a wrong value' });
+};
+
+module.exports = {
+  checkTaskDescription,
+  checkStatus,
+};
