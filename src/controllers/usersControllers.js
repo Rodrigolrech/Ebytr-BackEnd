a38fb3f12ba89ad8374dcc74998eb4b288d3a911
Rodrigo Rech <rodrigorech92@gmail.com>
2021-11-01 21:13:07 -0300

    Feat: toDoListService.insertNewTask || Fix: usersControllers eslint

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -4,10 +4,12 @@ const login = async (req, res) => {
   const { email, password } = req.body;
   const response = await usersService.login(email, password);
   return res.status(response.status).json(response.message);
-}
+};
 
 const createUser = async (req, res) => {
-  const { username, email, password, role } = req.body;
+  const {
+    username, email, password, role,
+  } = req.body;
   const response = await usersService.createUser(username, email, password, role);
   return res.status(response.status).json(response.message);
 };
